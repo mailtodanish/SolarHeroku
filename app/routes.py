761305ead185b0543ce386ce7cf52a28823e43de
MarkEhler <ehlerm42@gmail.com>
2019-09-06 10:59:49 -0600

    routing form to results

--- app/routes.py
+++ app/routes.py
@@ -24,7 +24,7 @@ def about():
 def handle_data():
 	projectpath = request.form
 	output, sunrise, sunset = loop_data_collect(int(request.form['time_span']), request.form['location'], request.form['date'])
-	day_dict = process(output, int(request.form['time_span'], sunrise, sunset)
+	day_dict = process(output, int(request.form['time_span']), sunrise, sunset)
 	return render_template('results.html', title=' sunny day(s)')
 
 
