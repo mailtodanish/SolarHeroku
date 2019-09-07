732d3b2b6b900f60aac4c211c2c7b2d88cd18ba7
Mohd Ahshan Danish <mailtodanish@gmail.com>
2019-09-07 15:27:32 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-from app import app
+from flask import Flask
+app = Flask(__name__)
 
-# app = create_app()
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
